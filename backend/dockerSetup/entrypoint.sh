61a049fcba6630d28c766917ce040ada7d213ac3
YoussefKhalidAli <154611350+YoussefKhalidAli@users.noreply.github.com>
2025-04-21 16:02:33 +0200

    Update entrypoint.sh

--- backend/dockerSetup/entrypoint.sh
+++ backend/dockerSetup/entrypoint.sh
@@ -1,34 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
-cd /var/www/lms
+set -e
 
-mkdir -p storage/logs bootstrap/cache database
-chmod -R 775 storage bootstrap/cache database
-chown -R www-data:www-data storage bootstrap/cache database
-
-touch storage/logs/laravel.log
-chown www-data:www-data storage/logs/laravel.log
-chmod 664 storage/logs/laravel.log
-
-if [ ! -f database/database.sqlite ]; then
-touch database/database.sqlite
-chown www-data:www-data database/database.sqlite
-chmod 664 database/database.sqlite
-fi
-
-if [ ! -d "vendor" ]; then
-composer install --no-interaction --prefer-dist
-fi
+chown -R www-data:www-data /var/www/html
+chmod -R 755 /var/www/html
 
 if [ ! -f .env ]; then
-cp .env.example .env
-php artisan key:generate
+    cp .env.example .env
+    php artisan key:generate --no-interaction
 fi
 
-php artisan key:generate
-php artisan migrate --force
-php artisan config:clear
-php artisan cache:clear
-php artisan route:clear
-
-exec php-fpm
+php-fpm -D
+exec nginx -g "daemon off;"
